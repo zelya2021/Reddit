d065e4474c74cb5aa4f1ad29a12c53c79999be81
zelya2021 <zelenckina1999@gmail.com>
2020-08-24 16:21:07 +0300

    trying to work with adapter

--- app/src/main/java/com/example/redditapp/MainActivity.java
+++ app/src/main/java/com/example/redditapp/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.redditapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import com.example.redditapp.adapters.InformationAdapter;
+import com.example.redditapp.models.Information;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView lvInfo;
+    ArrayList<Information> cars = new ArrayList<>();
+    {
+        for (int i = 1; i <= 10; i++) {
+            cars.add(new Information(String.format("author #%s", i), String.format("date #%s", i), String.format("picture #%s", i),String.format("comment #%s", i)));
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lvInfo=findViewById(R.id.lv_info);
+
+        InformationAdapter carsAdapter = new InformationAdapter(this, R.layout.publication_information, cars);
+        lvInfo.setAdapter(carsAdapter);
     }
 }
